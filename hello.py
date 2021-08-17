da4bb01ed6e8d6718ee544ed50a5c24029c9b3e8
Jamie Katsamatsas <jamie.kats@hotmail.com>
2021-08-17 19:04:43 +1000

    Added math

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 def function():
     print("Hello, World")
 
+    num1 = int(input("Add a numberator: "))
+    num2 = int(input("Add a second number: "))
+    print(num1/num2)
 
 if __name__ == '__main__':
     function()
